83d2cd80b16ae335dbd10673dafb2260f8d7be75
edlus2 <edgar.ludis2@gmail.com>
2024-01-03 10:03:22 -0300

    javascript

---
+++ javascript/aula1/primeiro_desafio.js
@@ -0,0 +1,12 @@
+const precoCombustivel = 5.79;
+const gastoMedio /*Gasto medio KM por litro*/ = 10;
+const distancia = 100;
+
+const listrosConsumido = distancia / gastoMedio;
+const gasto = listrosConsumido * precoCombustivel;
+
+console.log('o valor gasto sera de: '+gasto.toFixed(2));
+
+
+
+
